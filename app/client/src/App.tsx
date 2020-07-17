aeda7e9105eb0b574f9af63fa06887dc00b1e31b
Joseph Johny Emmatty <josinho.seven@gmail.com>
2020-07-17 05:55:21 +0530

    Added react app for client application

---
+++ app/client/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App: React.FunctionComponent = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>The sentinel client</p>
+      </header>
+    </div>
+  );
+};
+
+export default App;
