1e58503755ca73044256785c38ea933dc52359c9
Joseph Johny Emmatty <josinho.seven@gmail.com>
2020-07-30 20:10:31 +0530

    Authorize HOC code refactoring

--- app/client/src/components/auth/authorize.tsx
+++ app/client/src/components/auth/authorize.tsx
@@ -1,9 +1,5 @@
 import React from "react";
 
-interface Props {
-  Component: React.ComponentType<any | string>;
-}
-
 function Authorize<T>(Component: React.ComponentType<T>) {
   return (props: T) => <Component {...props} />;
 }
