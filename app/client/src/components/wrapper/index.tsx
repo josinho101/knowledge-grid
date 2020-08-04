5d6204a53f12e36b5cec06e68c049c398b023bb3
Joseph Johny Emmatty <josinho.seven@gmail.com>
2020-08-05 02:23:17 +0530

    Added wiki navigator

--- app/client/src/components/wrapper/index.tsx
+++ app/client/src/components/wrapper/index.tsx
@@ -2,19 +2,28 @@ import React from "react";
 import Footer from "../footer";
 import Header from "../../components/header";
 import SiteNavigator from "../navigator/sitenavigator";
+import WikiNavigator from "../navigator/wikinavigator";
+import SplitPane from "react-split-pane";
 
 const ContentWrapper: React.FC<{}> = (props) => {
   return (
     <React.Fragment>
       <Header />
       <div className="content-wrapper">
-        <SiteNavigator />
-        <div id="content-wrapper" className="d-flex flex-column">
-          <div id="content">
-            <div className="container-fluid">{props.children}</div>
+        <SplitPane
+          split="vertical"
+          minSize={100}
+          defaultSize={200}
+          maxSize={500}
+        >
+          <WikiNavigator />
+          <div id="content-wrapper" className="d-flex flex-column">
+            <div id="content">
+              <div className="container-fluid">{props.children}</div>
+            </div>
+            <Footer />
           </div>
-          <Footer />
-        </div>
+        </SplitPane>
       </div>
     </React.Fragment>
   );
