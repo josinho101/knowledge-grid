90514958db830737258a297de74189dff94be398
Joseph Johny Emmatty <josinho.seven@gmail.com>
2020-08-06 05:55:49 +0530

    Added button on wiki pages

--- app/client/src/components/dashboard/index.tsx
+++ app/client/src/components/dashboard/index.tsx
@@ -4,6 +4,14 @@ const Dashboard: React.FC = () => {
   return (
     <div className="d-sm-flex align-items-center justify-content-between mb-4">
       <h1 className="h3 mb-0 text-gray-800 font-weight-normal">Dashboard</h1>
+      <div>
+        <button type="button" className="btn btn-primary btn-sm m-1">
+          Add item
+        </button>
+        <button type="button" className="btn btn-primary btn-sm m-1">
+          Edit
+        </button>
+      </div>
     </div>
   );
 };
