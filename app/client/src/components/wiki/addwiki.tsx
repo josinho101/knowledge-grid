2268bbb0f72316461a906bd04b6bd04f50426fa2
Joseph Johny Emmatty <josinho.seven@gmail.com>
2020-08-07 19:21:34 +0530

    Added controls in add new wiki page

--- app/client/src/components/wiki/addwiki.tsx
+++ app/client/src/components/wiki/addwiki.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import WikiTree from "./wikitree";
+import Textbox from "../common/textbox";
+import Checkbox from "../common/checkbox";
 import CommonModal from "../common/commonmodal";
 import localeHelper from "../../utils/localehelper";
-import Textbox from "../common/textbox";
 
 interface Props {
   isOpen: boolean;
@@ -33,8 +34,73 @@ const AddWiki: React.FC<Props> = (props) => {
         </div>
         <div className="wiki-add-right-pane">
           <div className="form-group">
-            <label className="col-form-label">Parent - &nbsp;</label>
-            <b>Selected wiki folder</b>
+            <label className="col-form-label">
+              {localeHelper.translate("pages.wiki.add-new-modal.parent")} -
+              &nbsp;
+            </label>
+            <b>{"<SELECTED WIKI FOLDER>"}</b>
+          </div>
+          <div className="form-group">
+            <div className="form-row">
+              <div className="col">
+                <label className="col-form-label">
+                  {localeHelper.translate("pages.wiki.add-new-modal.wiki-type")}
+                </label>
+              </div>
+              <div className="col">
+                <div className="dropdown">
+                  <a
+                    className="btn btn-outline-secondary dropdown-toggle"
+                    href="#"
+                    role="button"
+                    id="dropdownMenuLink"
+                    data-toggle="dropdown"
+                    aria-haspopup="true"
+                    aria-expanded="false"
+                  >
+                    {localeHelper.translate(
+                      "pages.wiki.add-new-modal.wiki-type-dropdown.none"
+                    )}
+                  </a>
+
+                  <div
+                    className="dropdown-menu"
+                    aria-labelledby="dropdownMenuLink"
+                  >
+                    <a className="dropdown-item" href="#">
+                      {localeHelper.translate(
+                        "pages.wiki.add-new-modal.wiki-type-dropdown.folder"
+                      )}
+                    </a>
+                    <a className="dropdown-item" href="#">
+                      {localeHelper.translate(
+                        "pages.wiki.add-new-modal.wiki-type-dropdown.page"
+                      )}
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div className="col"></div>
+              <div className="col"></div>
+            </div>
+          </div>
+          <div className="form-group">
+            <label className="col-form-label">
+              {localeHelper.translate("pages.wiki.add-new-modal.wiki-title")}
+            </label>
+            <Textbox id="wiki-title" type="text" className="form-control" />
+          </div>
+          <div className="form-group">
+            <div className="m-4">
+              <Checkbox
+                id="rememberMe"
+                label={localeHelper.translate(
+                  "pages.wiki.add-new-modal.edit-on-save"
+                )}
+                className="custom-control-input"
+                labelClassName="custom-control-label"
+              />
+            </div>
           </div>
         </div>
       </div>
