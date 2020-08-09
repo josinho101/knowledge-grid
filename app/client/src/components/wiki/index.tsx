ed1934ef0180039c1a5387d75cf31fbc0df5dba9
Joseph Johny Emmatty <josinho.seven@gmail.com>
2020-08-09 10:07:05 +0530

    Added default message for wiki page

--- app/client/src/components/wiki/index.tsx
+++ app/client/src/components/wiki/index.tsx
@@ -1,10 +1,10 @@
 import AddWiki from "./addwiki";
+import EditWiki from "./editwiki";
 import React, { useState } from "react";
 import Dropdown from "react-bootstrap/esm/Dropdown";
 import localeHelper from "../../utils/localehelper";
 import DropdownButton from "react-bootstrap/DropdownButton";
 import { DropdownItemProps } from "react-bootstrap/esm/DropdownItem";
-import EditWiki from "./editwiki";
 
 const WikiPage: React.FC = () => {
   const [doShowAddNewModal, setShowAddNewModal] = useState(false);
@@ -37,27 +37,32 @@ const WikiPage: React.FC = () => {
   };
 
   return (
-    <div className="d-sm-flex align-items-center justify-content-between mb-4">
-      <h1 className="h3 mb-0 text-gray-800 font-weight-normal">Wiki Title</h1>
-      <DropdownButton id="dropdown-basic-button" title="Actions" size="sm">
-        <Dropdown.Item onClick={onEditClick}>
-          {localeHelper.translate("pages.wiki.edit-btn")}
-        </Dropdown.Item>
-        <Dropdown.Item onClick={onAddNewClick}>
-          {localeHelper.translate("pages.wiki.add-new-btn")}
-        </Dropdown.Item>
-      </DropdownButton>
-      <AddWiki
-        isOpen={doShowAddNewModal}
-        onSaveClick={onSaveClick}
-        onCancelClick={onCancelClick}
-      />
-      <EditWiki
-        isOpen={doShowEditModal}
-        onSaveClick={onEditSaveClick}
-        onCancelClick={onEditCancelClick}
-      />
-    </div>
+    <React.Fragment>
+      <div className="d-sm-flex align-items-center justify-content-between mb-4">
+        <h1 className="h3 mb-0 text-gray-800 font-weight-normal">
+          {localeHelper.translate("pages.wiki.default-title")}
+        </h1>
+        <DropdownButton id="dropdown-basic-button" title="Actions" size="sm">
+          <Dropdown.Item onClick={onEditClick}>
+            {localeHelper.translate("pages.wiki.edit-btn")}
+          </Dropdown.Item>
+          <Dropdown.Item onClick={onAddNewClick}>
+            {localeHelper.translate("pages.wiki.add-new-btn")}
+          </Dropdown.Item>
+        </DropdownButton>
+        <AddWiki
+          isOpen={doShowAddNewModal}
+          onSaveClick={onSaveClick}
+          onCancelClick={onCancelClick}
+        />
+        <EditWiki
+          isOpen={doShowEditModal}
+          onSaveClick={onEditSaveClick}
+          onCancelClick={onEditCancelClick}
+        />
+      </div>
+      <p>{localeHelper.translate("pages.wiki.default-content")}</p>
+    </React.Fragment>
   );
 };
 
