db693649a4ae0be4260074facc51351efb686158
Joseph Johny Emmatty <josinho.seven@gmail.com>
2020-09-04 05:50:44 +0530

    Added delete modal for wiki

---
+++ app/client/src/components/wiki/deletewiki.tsx
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import { Wiki } from "../../models/wiki";
+import { AppState } from "../../reducers";
+import { deleteWiki } from "../../actions/wiki";
+import { useSelector, connect } from "react-redux";
+import localeHelper from "../../utils/localehelper";
+import ConfirmationModal from "../common/confirmationmodal";
+
+interface Props {
+  isOpen: boolean;
+  wiki?: Wiki;
+  deleteWiki: Function;
+  onCancelClick: (e: React.MouseEvent<HTMLElement, MouseEvent>) => void;
+  onDeleteClick: (e: React.MouseEvent<HTMLElement, MouseEvent>) => void;
+}
+
+const deleteWikiModal: React.FC<Props> = (props) => {
+  const token = useSelector((state: AppState) => state.auth.token);
+  const selectedWiki = useSelector(
+    (state: AppState) => state.data.selectedWiki
+  );
+
+  const onCancelClick = (e: React.MouseEvent<HTMLElement, MouseEvent>) => {};
+
+  const onDeleteClick = (e: React.MouseEvent<HTMLElement, MouseEvent>) => {};
+
+  const getContent = () => {
+    const content = localeHelper.translate("pages.wiki.delete-modal.content");
+    return `${content} ${selectedWiki?.title}`;
+  };
+
+  return (
+    <ConfirmationModal
+      id="delete-wiki-modal"
+      isOpen={props.isOpen}
+      content={getContent()}
+      primaryButtonText={localeHelper.translate(
+        "pages.wiki.delete-modal.delete-btn"
+      )}
+      secondaryButtonText={localeHelper.translate(
+        "pages.wiki.delete-modal.cancel-btn"
+      )}
+      closeButtonClickHandler={onCancelClick}
+      primaryButtonClickHandler={onDeleteClick}
+      secondaryButtonClickHandler={onCancelClick}
+      title={localeHelper.translate("pages.wiki.delete-modal.title")}
+    />
+  );
+};
+
+const mapDispatchToProps = {
+  deleteWiki,
+};
+
+export default connect(undefined, mapDispatchToProps)(deleteWikiModal);
